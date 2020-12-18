a60cf843727a859d7b090a6c1af73f425783bd28
bpilosta <bpilosta@foi.hr>
2020-12-18 15:27:39 +0100

    Added delete and edit icons to Slidable widget

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,5 +1,6 @@
 import 'package:air_2011/providers/app_user.dart';
 import 'package:air_2011/screens/add_order_screen.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:air_2011/screens/order_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -10,40 +11,87 @@ class OrderItem extends StatelessWidget {
   final Order _thisOrder;
   final AppUser _loggedUser;
   OrderItem(this._thisOrder, this._loggedUser);
+
+//Handles what happens if the delete button has been clicked
+  Future<void> _deleteHandler(context) {
+    return showDialog<void>(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            title: Text("Delete order?"),
+            content: Text("Are you sure you want to delete this order?"),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    //Need to implement delete functionality
+                    //Right now prints buyer's surname
+                    debugPrint(_thisOrder.buyer.surname);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Yes")),
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text("No")),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        onTap: () {
-          Navigator.of(context).pushNamed(OrderDetailScreen.routeName,
-              arguments: [_thisOrder, _loggedUser.userType]);
-        },
-        leading: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Date', style: Theme.of(context).textTheme.subtitle1),
-            Container(
-              margin: EdgeInsets.only(top: 2),
-              child: Text(DateFormat('dd/MM/yyyy').format(_thisOrder.orderDate),
-                  style: Theme.of(context).textTheme.caption),
-            ),
-          ],
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.20,
+      actions: [
+        IconSlideAction(
+          caption: 'Edit',
+          color: Theme.of(context).accentColor,
+          icon: Icons.edit,
+          //Need to add onTap functionality
+          onTap: () => null,
         ),
-        title: Text('Buyer'),
-        subtitle: Text('${_thisOrder.buyer.name} ${_thisOrder.buyer.surname}'),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              _thisOrder.finished
-                  ? Icons.check_box
-                  : Icons.check_box_outline_blank,
-              color: Colors.green,
-            ),
-            Icon(
-              Icons.notifications,
-              color: Theme.of(context).accentColor,
-            )
-          ],
-        ));
+        IconSlideAction(
+          caption: 'Delete',
+          color: Theme.of(context).errorColor,
+          icon: Icons.delete,
+          onTap: () => _deleteHandler(context),
+        )
+      ],
+      child: ListTile(
+          onTap: () {
+            Navigator.of(context).pushNamed(OrderDetailScreen.routeName,
+                arguments: [_thisOrder, _loggedUser.userType]);
+          },
+          leading: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Date', style: Theme.of(context).textTheme.subtitle1),
+              Container(
+                margin: EdgeInsets.only(top: 2),
+                child: Text(
+                    DateFormat('dd/MM/yyyy').format(_thisOrder.orderDate),
+                    style: Theme.of(context).textTheme.caption),
+              ),
+            ],
+          ),
+          title: Text('Buyer'),
+          subtitle:
+              Text('${_thisOrder.buyer.name} ${_thisOrder.buyer.surname}'),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                _thisOrder.finished
+                    ? Icons.check_box
+                    : Icons.check_box_outline_blank,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.notifications,
+                color: Theme.of(context).accentColor,
+              )
+            ],
+          )),
+    );
   }
 }
